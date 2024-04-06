d947b270f01ed06d84745836e57e5e887a827977
Tostiffent <uselessm69@gmail.com>
2024-04-06 10:34:37 +0530

    folders for basic UI elements and project structure

---
+++ src/app/crop/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.css";
+
+export default function Crop() {
+  return <div className={styles.container}>Crop UI Here</div>;
+}
---
+++ src/app/map/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.css";
+
+export default function Map() {
+  return <div className={styles.container}>Render map here!</div>;
+}
