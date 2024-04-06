708a1a826ba330f45c23b1c07bd3bc90f5fd9dbb
sulaiman-shariff <sulaiman05221@gmail.com>
2024-04-06 10:47:41 +0530

    Added landing page video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <div>Landing page</div>;
+  return (
+    <div className="relative w-full h-[90vh] flex flex-row justify-center">
+            <video
+        className="w-full absolute"
+                muted={true}
+                autoPlay={true}
+        loop={true}
+        preload="auto"
+                src="/hero_video.mp4"
+            ></video>
+    <h1 className="text-8xl text-white absolute bottom-28 font-bold">Project Sunroof India</h1>
+        </div>
+  )
 }
